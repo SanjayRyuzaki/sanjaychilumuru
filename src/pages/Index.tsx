799c0b0a7f5dd7da416e1b51b5f32fee70127fc3
SanjayRyuzaki <106882956+SanjayRyuzaki@users.noreply.github.com>
2025-08-19 23:57:34 +0530

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,6 +4,7 @@ import About from '@/components/About';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
 import Contact from '@/components/Contact';
+import BlogSection from '@/components/BlogSection';
 
 const Index = () => {
   return (
@@ -13,6 +14,7 @@ const Index = () => {
         <Hero />
         <About />
         <Projects />
+        <BlogSection />
         <Skills />
         <Contact />
       </main>
